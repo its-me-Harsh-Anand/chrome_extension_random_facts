43076a6b8ac65fee94e43cecc68e2ae3cc86e86f
gokulakrishnan-shankar <gokullan1101@gmail.com>
2022-10-06 23:55:16 +0530

    feat: dynamic background color change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 fetch('https://api.api-ninjas.com/v1/facts?limit=1', {
     headers: {
-        'X-Api-Key': 'Your API key for API Ninjas'
+        'X-Api-Key': '0ZcIYI+egt8q6UUI+uZ9dQ==0Gi2gQdjzQEwtaed'
       },
 })
     .then(res => res.json())
@@ -8,6 +8,7 @@ fetch('https://api.api-ninjas.com/v1/facts?limit=1', {
         const fact = data[0]?.fact
         const para = document.querySelector('p')
         para.innerHTML = fact || "Retry..."
+        change_color(para);
         wow();
     })
     .catch(err => {
@@ -17,4 +18,27 @@ fetch('https://api.api-ninjas.com/v1/facts?limit=1', {
 function wow() {
     var snd = new Audio('./sound.mp3')
     snd.play();
+}
+
+function change_color(paraNode) {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    if (randomColor.length < 6) {
+        randomColor = "0" + randomColor;
+    }
+
+    checkColors("ffffff", randomColor);
+    var whiteForegoundContrast = parseFloat(colorData['contrast']);
+    checkColors("000000", randomColor);
+    var blackForegoundContrast = parseFloat(colorData['contrast']);
+
+    // console.log(whiteForegoundContrast + " " + blackForegoundContrast);
+
+    if (whiteForegoundContrast < blackForegoundContrast) {
+        paraNode.style.color = "#000000";
+    }
+    else {
+        paraNode.style.color = "#ffffff";
+    }
+
+    document.querySelector('body').style.backgroundColor = "#"+randomColor;    ;
 }
